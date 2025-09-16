e02dd8113648302d6f3ca5f37bdd3460182e5f00
Matheus Marinho <marinho2matheus@gmail.com>
2025-09-16 19:48:34 -0300

    código se encontre

---
+++ App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class App
+{
+	public static void main(String[] args) {
+	    String salaDesejada = "A219";
+	    String salaInformada = "";
+	    Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o numero da sala: ");
+		salaInformada = scanner.next();
+		if(salaDesejada.equals(salaInformada)){
+		    System.out.println("Você encontrou sua sala!");
+		}else{
+		    System.out.println("Você está perdido!");
+		}
+		scanner.close();
+	}
+}
